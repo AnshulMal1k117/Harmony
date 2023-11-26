a715cd2474d51fef7e526b92601a1182489b03c4
AnshulMal1k117 <129662683+AnshulMal1k117@users.noreply.github.com>
2023-11-26 17:47:27 +0530

    Updated UI

---
+++ app/src/main/java/com/example/harmony/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.harmony
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setTheme(R.style.Base_Theme_Harmony)
+        setContentView(R.layout.activity_player)
+    }
+}
