02ade0823809d5633e5159082e6014266da5beff
AnshulMal1k117 <129662683+AnshulMal1k117@users.noreply.github.com>
2023-12-13 23:34:45 +0530

    Added Song Duration

--- app/src/main/java/com/example/harmony/adapter/MusicAdapter.kt
+++ app/src/main/java/com/example/harmony/adapter/MusicAdapter.kt
@@ -1,13 +1,17 @@
 package com.example.harmony.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.harmony.PlayerActivity
 import com.example.harmony.R
 import com.example.harmony.data.Music
+import com.example.harmony.data.formatDuration
 import com.example.harmony.databinding.MusicViewBinding
 
 class MusicAdapter(private val context: Context, private val musicList: ArrayList<Music>):RecyclerView.Adapter<MusicAdapter.MusicHolder>() {
@@ -16,6 +20,7 @@ class MusicAdapter(private val context: Context, private val musicList: ArrayLis
         val album = binding.albumNameMusicView
         val image = binding.imageMusicView
         val duration = binding.songDurationMusicView
+        val root = binding.root
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicAdapter.MusicHolder {
@@ -25,11 +30,15 @@ class MusicAdapter(private val context: Context, private val musicList: ArrayLis
     override fun onBindViewHolder(holder: MusicAdapter.MusicHolder, position: Int) {
         holder.title.text = musicList[position].title
         holder.album.text = musicList[position].album
-        holder.duration.text = musicList[position].duration.toString()
+        holder.duration.text = formatDuration(musicList[position].duration)
         Glide.with(context)
             .load(musicList[position].artUri)
             .apply(RequestOptions().placeholder(R.drawable.harmony_logo_splash_screen).centerCrop())
             .into(holder.image)
+        holder.root.setOnClickListener{
+            val intent = Intent(context, PlayerActivity::class.java)
+            ContextCompat.startActivity(context, intent, null)
+        }
     }
 
     override fun getItemCount(): Int {
