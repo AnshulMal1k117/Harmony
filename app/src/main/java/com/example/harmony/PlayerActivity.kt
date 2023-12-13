85fcfcb9c8fee192bdac53d23242df79e16bdfda
AnshulMal1k117 <129662683+AnshulMal1k117@users.noreply.github.com>
2023-12-13 23:59:43 +0530

    Added Media Player

--- app/src/main/java/com/example/harmony/PlayerActivity.kt
+++ app/src/main/java/com/example/harmony/PlayerActivity.kt
@@ -1,10 +1,18 @@
 package com.example.harmony
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.harmony.data.Music
 import com.example.harmony.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
+
+    companion object{
+        lateinit var musicListPlayerActivity: ArrayList<Music>
+        var songPosition:Int = 0
+        var mediaPlayer: MediaPlayer? = null
+    }
 
     private lateinit var binding: ActivityPlayerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +20,16 @@ class PlayerActivity : AppCompatActivity() {
         setTheme(R.style.Base_Theme_Harmony)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        songPosition = intent.getIntExtra("index", 0)
+        when(intent.getStringExtra("class")) {
+            "MusicAdapter" -> {
+                musicListPlayerActivity = ArrayList()
+                musicListPlayerActivity.addAll(MainActivity.MusicListMainActivity)
+                if(mediaPlayer == null) mediaPlayer = MediaPlayer()
+                mediaPlayer!!.reset()
+                mediaPlayer!!.setDataSource(musicListPlayerActivity[songPosition].path)
+            }
+        }
+
     }
 }
